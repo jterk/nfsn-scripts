921a66c31dbf9b807ad35addc56fd834668e06a2
Jason Terk <jason@goterkyourself.com>
2017-09-04 11:53:56 -0700

    Compress DB Backup
    Closes #3

--- dbbackup.sh
+++ dbbackup.sh
@@ -3,7 +3,8 @@
 # Invoked as HOST=<sql host> USER=<sql user> S3_BUCKET=<s3 bucket> dbbackup.sh
 #
 LOGFILE=/home/logs/dbbackup.log
+BACKUP_FILE=/tmp/$HOST.sql.bz2
 
-mysqldump -h $HOST -u $USER --all-databases > /tmp/$HOST.sql
-s3cmd -q put /tmp/$HOST.sql s3://$S3_BUCKET/ 1> $LOGFILE 2>&1 | grep -n "Owner groupname not known"
-rm /tmp/$HOST.sql 1>> $LOGFILE
+mysqldump -h $HOST -u $USER --all-databases | bzip2 > $BACKUP_FILE
+s3cmd -q put /tmp/$HOST.sql.bz2 s3://$S3_BUCKET/ 1> $LOGFILE 2>&1 | grep -n "Owner groupname not known"
+rm $BACKUP_FILE 1>> $LOGFILE
