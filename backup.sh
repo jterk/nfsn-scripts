ea61f93d186797b9432ad5ffd475f32bc6a95428
Jason Terk <jason@goterkyourself.com>
2017-09-04 11:47:30 -0700

    Compress Archives
    Closes #1

--- backup.sh
+++ backup.sh
@@ -2,9 +2,9 @@
 #
 # Invoked as S3_BUCKET=<bucket name> backup.sh
 #
-TARFILE=/tmp/$NFSN_SITE_NAME.tar
+TARFILE=/tmp/$NFSN_SITE_NAME.tbz2
 LOGFILE=/home/logs/backup.log
 
-tar -cf $TARFILE --exclude home/tmp --exclude home/private/.cache --exclude home/private/.local -C / home 1> $LOGFILE
+tar -cjf $TARFILE --exclude home/tmp --exclude home/private/.cache --exclude home/private/.local -C / home 1> $LOGFILE
 s3cmd put $TARFILE s3://$S3_BUCKET/ 1>> $LOGFILE 2>&1 | grep -n "Owner groupname not known"
 rm $TARFILE 1>> $LOGFILE
